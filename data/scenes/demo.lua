af1bef8b56f42b9fa9b404f115942467ccb37dff
IcarusIsDead <163286125+IcarusIsDead@users.noreply.github.com>
2026-05-13 11:39:33 +0800

    removed the timer from player entity, added /bigobj

--- data/scenes/demo.lua
+++ data/scenes/demo.lua
@@ -64,17 +64,6 @@ scene = {
         fileName = "playerInput.lua",
         filePath = "..\\data\\luaScripts\\"
       } --[[table: 000001EE2719E6D0]],
-	    {
-        Name = "Script",
-        fileName = "incrementTimer.lua",
-        filePath = "..\\data\\luaScripts\\"
-      },
-	    {
-        Name = "Script",
-        fileName = "timer.lua",
-        filePath = "..\\data\\luaScripts\\",
-		    timer = "0"
-      }--[[table: 0000012BE60538A0]],
       {
         Name = "Script",
         fileName = "physicsTriggers.lua",
