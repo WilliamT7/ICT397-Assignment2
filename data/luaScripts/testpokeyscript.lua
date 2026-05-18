d0927d742f6627435d469a4a33d8aafba67966f5
Asch <kay.bradsell@gmail.com>
2026-05-18 20:54:11 +0800

    temp

--- data/luaScripts/testpokeyscript.lua
+++ data/luaScripts/testpokeyscript.lua
@@ -1,12 +1,15 @@
 local speed = 1
+local randomDir
+
+function start()
+    -- pick a direction Vector2().normalize so x and y are rand  1-10
+    local randomX = math.random(-5,5)
+    local randomZ = math.random(-5,5)
+    local randomDir = Vector3.new(randomX, 0, randomZ)
+    randomDir:normalize
+end
 
 function update()
-    -- pick a direction Vector2().normalize so x and y are rand  1-10
-    --local randomX = math.random(-5,5)
-    --local randomZ = math.random(-5,5)
-    --local randomDir = Vector3.new(randomX, 0, randomZ)
-    local randomDir = Vector3.new(0, 0, 1)
-
     -- increase transform for that x/z by 1
     local transform = getTransform(obj)
     local animation = getAnimation(obj)
---
+++ data/prefabs/prefabpokey.lua
@@ -0,0 +1,31 @@
+entity =
+  {
+    Components = {
+      {
+        Name = "Transform",
+        position_x = 0.60000002384185791,
+        position_y = 0,
+        position_z = -4.8000001907348633,
+        rotation_x = 0,
+        rotation_y = 0,
+        rotation_z = 0,
+        scale_x = 2,
+        scale_y = 2,
+        scale_z = 2
+      } --[[table: 0000021755630250]],
+      {
+        ModelID = "SPIKY",
+        Name = "MeshRenderer",
+        ShaderID = 3
+      } --[[table: 0000021755631250]],
+      {
+        Name = "Animation"
+      },
+      {
+        Name = "Script",
+        fileName = "testpokeyscript.lua",
+        filePath = "..\\data\\luaScripts\\"
+      }
+    } --[[table: 000002175562FB50]],
+    EntityName = "Spiky"
+  }
