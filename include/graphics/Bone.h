e1352455a44086ca781584a6680c22fbb9974bf6
Kay Bradsell <kay.bradsell@gmail.com>
2026-05-27 16:20:06 +0800

    Doxgeyn for bone

--- include/graphics/Bone.h
+++ include/graphics/Bone.h
@@ -20,14 +20,9 @@
 
 namespace Graphics
 {
-	/**
+	/** Key Position
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
-		*
-		* @pre -
-		* @post -
+		* @brief - Struct that stores a vector3 and timestamp
 		**/
 	struct KeyPosition
 	{
@@ -35,14 +30,9 @@ namespace Graphics
 		float timestamp;
 	};
 
-	/**
+	/** Key Rotation
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
-		*
-		* @pre -
-		* @post -
+		* @brief -  Struct that stores quaternion rotation and timestamp
 		**/
 	struct KeyRotation
 	{
@@ -50,14 +40,9 @@ namespace Graphics
 		float timestamp;
 	};
 
-	/**
+	/** Key Scale
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
-		*
-		* @pre -
-		* @post -
+		* @brief - Struct that stores vec3 scale and timestamp
 		**/
 	struct KeyScale
 	{
@@ -68,91 +53,88 @@ namespace Graphics
 	class Bone
 	{
 	public:
-		/**
+		/** Bone Constructor
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - Constructs Bone and loads in data
+		* @param - std::string name
+		* @param - int ID
+		* @param - aiNodeAnim* channel
 		*
-		* @pre -
-		* @post -
+		* @pre - all parameters have valid data
+		* @post - loads in and constructs this bone
 		**/
 		Bone(const std::string& name, int ID, const aiNodeAnim* channel);
 
-		/**
+		/** Update
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - given animTime, update the bones transforms with interpolation
+		* @param - float animTime
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed
+		* @post - Updates the bones position based on time
 		**/
 		void Update(float animTime);
 
-		/**
+		/** Get Local Transform
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - Returns the local transform for the bone
+		* @return - glm::mat4
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed
+		* @post - Returns the current local transformation for this bone
 		**/
 		glm::mat4 GetLocalTransform() const;
 
-		/**
+		/** Get Bone Name
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - Returns the name for this bone
+		* @return - std::string
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed
+		* @post - Returns the name of the bone
 		**/
 		std::string GetBoneName() const;
 
-		/**
+		/** Get Bone ID
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - returns the ID for this bone
+		* @return - int
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed
+		* @post - Returns the ID of this bone
 		**/
 		int GetBoneID() const;
 
-		/**
+		/** Get Position index
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - returns the position index for the current animationTime
+		* @param - float animationTime
+		* @return - int
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed, and animationTime is within animationData
+		* @post - Returns the index for the bone given time, else has assertion,
 		**/
 		int GetPositionIndex(float animationTime) const;
 
-		/**
+		/** Get Rotation index
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - returns the rotation index for the current animationTime
+		* @param - float animationTime
+		* @return - int
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed, and animationTime is within animationData
+		* @post - Returns the index for the bone given time, else has assertion,
 		**/
 		int GetRotationIndex(float animationTime) const;
 
-		/**
+		/** Get Scale index
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - returns the scale index for the current animationTime
+		* @param - float animationTime
+		* @return - int
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed, and animationTime is within animationData
+		* @post - Returns the index for the bone given time, else has assertion,
 		**/
 		int GetScaleIndex(float animationTime) const;
 
@@ -169,47 +151,48 @@ namespace Graphics
 		std::string m_name;
 		int m_ID;
 		
-		/**
+		/** Get Scale Factor
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief - Returns the time scale for animation
+		* @param - float lastTime
+		* @param - float nextTime
+		* @return - float
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed
+		* @post - Returns float time scale
 		**/
 		float GetScaleFactor(float lastTime, float nextTime, float animationTime);
 
-		/**
+		/** Interpolate Position
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief  returns the mat4 for the position given animationTime
+		* @param - float animationTime
+		* @return - glm::mat4
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed, and animationTime is within the time bounds of bone
+		* @post - returns the mat4 transformation for position
 		**/
 		glm::mat4 InterpolatePosition(float animationTime);
 
-		/**
+		/** Interpolate Rotation
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief  returns the mat4 for the rotation given animationTime
+		* @param - float animationTime
+		* @return - glm::mat4
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed, and animationTime is within the time bounds of bone
+		* @post - returns the mat4 transformation for rotation
 		**/
 		glm::mat4 InterpolateRotation(float animationTime);
 
-		/**
+		/** Interpolate Scale
 		* @author - Kay Bradsell
-		* @brief -
-		* @param -
-		* @return -
+		* @brief  returns the mat4 for the scale given animationTime
+		* @param - float animationTime
+		* @return - glm::mat4
 		*
-		* @pre -
-		* @post -
+		* @pre - Bone has been constructed, and animationTime is within the time bounds of bone
+		* @post - returns the mat4 transformation for scale
 		**/
 		glm::mat4 InterpolateScale(float animationTime);
 	};
