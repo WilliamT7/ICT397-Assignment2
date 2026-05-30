9e9a55f8fbdadfa6f6a8918d7af4cbc6563389cf
Asch <kay.bradsell@gmail.com>
2026-05-30 20:26:38 +0800

    exit game added

--- data/luaScripts/ui/mainmenumenu.lua
+++ data/luaScripts/ui/mainmenumenu.lua
@@ -5,5 +5,6 @@ function render()
     GUIBegin("Super Goober Bros World 2 HD")
     GUIMakeButton("START GAME", LoadScene, start)
     GUIMakeButton("LOAD GAME", LoadScene, load)
+    GUIMakeButton("EXIT", CloseProgram)
     GUIEnd()
 end
