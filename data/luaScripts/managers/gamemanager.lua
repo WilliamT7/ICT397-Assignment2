db6197e34c22b16f6e323b440a34e3471ad3ec86
IcarusIsDead <163286125+IcarusIsDead@users.noreply.github.com>
2026-05-30 17:02:53 +0800

    set amountToSpawn back to 5 not 25

--- data/luaScripts/managers/gamemanager.lua
+++ data/luaScripts/managers/gamemanager.lua
@@ -8,7 +8,7 @@ function update()
 	local currentWave = tonumber(vars:getGlobal("currentWave").value)
 	local numEnemiesSpawned = tonumber(vars:getGlobal("numEnemiesSpawned").value)
 
-	local amountToSpawn = currentWave * 25
+	local amountToSpawn = currentWave * 5
 	if numEnemiesSpawned < amountToSpawn then
 		SpawnEnemy(vars)
 	end
