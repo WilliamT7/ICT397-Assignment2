74d9074a0bc738d00db56fe9f076d49cba4865ca
WilliamT7 <williamdj03@outlook.com>
2026-06-01 23:51:59 +0800

    terrintoggle cleanup ;(

--- data/luaScripts/terrain/terraintoggle.lua
+++ data/luaScripts/terrain/terraintoggle.lua
@@ -1,21 +1,29 @@
+local terrain = nil
+
 function update()
     terrainToggle()
 end
 
-function terrainToggle()
-	local terrain = nil
-
-	if getTerrain ~= nil then
+function getTerrainComponent()
+	if terrain == nil and getTerrain ~= nil then
 		terrain = getTerrain(obj)
 	end
+
+	return terrain
+end
+
+function terrainToggle()
+	if not GetKeyPress(11) then
+		return
+	end
+
+	local terrain = getTerrainComponent()
 
 	if terrain == nil then
 		return
 	end
 
 	-- Wireframe toggle
-	if GetKeyPress(11) then
-		print("LUA: K (wire toggle)")
-		terrain:toggleWireframe()
-	end
+	print("LUA: K (wire toggle)")
+	terrain:toggleWireframe()
 end
