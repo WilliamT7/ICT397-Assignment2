ad9c9a943c4d66146daa5984b3b8449f3d169146
Asch <kay.bradsell@gmail.com>
2026-06-02 01:34:30 +0800

    forgot to update the prefab

--- data/prefabs/prefabpokey.lua
+++ data/prefabs/prefabpokey.lua
@@ -1,25 +1,44 @@
-entity =
-  {
-    Components = {
+data = {
+  pooling = 100,
+  entity =
+    {
+      Components = {
+        {
+          Name = "Transform",
+          position_x = 0.60000002384185791,
+          position_y = 0,
+          position_z = -4.8000001907348633,
+          rotation_x = 0,
+          rotation_y = 0,
+          rotation_z = 0,
+          scale_x = 1,
+          scale_y = 1,
+          scale_z = 1
+        } --[[table: 0000021755630250]],
+        {
+          ModelID = "SPIKY",
+          Name = "MeshRenderer",
+          ShaderID = 3
+        } --[[table: 0000021755631250]],
+        {
+          Name = "Animation"
+        },
+        {
+          Name = "Physics",
+          halfExtents_x = 3,
+          halfExtents_y = 20,
+          halfExtents_z = 3,
+          centerOffset_x = 0,
+          centerOffset_y = 20,
+          centerOffset_z = 0,
+          isStatic = false,
+          useGravity = true,
+          useCCD = false,
+          mass = 1,
+          shape = "capsule"
+        },
       {
-        Name = "Transform",
-        position_x = 0.60000002384185791,
-        position_y = 0,
-        position_z = -4.8000001907348633,
-        rotation_x = 0,
-        rotation_y = 0,
-        rotation_z = 0,
-        scale_x = 1,
-        scale_y = 1,
-        scale_z = 1
-      } --[[table: 0000021755630250]],
-      {
-        ModelID = "SPIKY",
-        Name = "MeshRenderer",
-        ShaderID = 3
-      } --[[table: 0000021755631250]],
-      {
-        Name = "Animation"
+      Name = "FSM"
       },
       {
         Name = "Physics",
@@ -66,3 +85,5 @@ entity =
     } --[[table: 000002175562FB50]],
     EntityName = "Spiky"
   }
+}
+
